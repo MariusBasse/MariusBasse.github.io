dab3473ddb62e5885f035d5c471cf619d0ecc6e2
MariusBasse <marius.basse@live.co.uk>
2020-09-27 20:57:10 +0200

    2

--- src/components/header.js
+++ src/components/header.js
@@ -5,38 +5,39 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `black`,
       marginBottom: `1.45rem`,
+      position: `sticky`,
+      top: `0`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        maxWidth: 1300,
+        padding: `1.45rem 0`,
+        maxHeight: `70px`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+      <div>
+        <h1 style={{ margin: 0, fontSize: `20px`, alignItems: `left` }}>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            Home
+          </Link>
+        </h1>
+      </div>
     </div>
   </header>
 )
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
 }
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,35 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import ich from "../images/ich.jpeg"
+import ich2 from "../images/ich2.jpeg"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Marius</h1>
-    <p>Welcome to your sfsnew Gatsby site.</p>
+    <h1>Marius Basse Webpage</h1>
+    <>
+      <div style={{ display: "flex", flexWrap: "wrap", padding: "0 4px" }}>
+        <div style={{ flex: "33%", maxWidth: "33%", padding: "0 4px" }}>
+          <img
+            style={{
+              verticalAlign: "middle",
+              width: "100%",
+            }}
+            src={ich}
+          />
+        </div>
+        <div style={{ flex: "33%", maxWidth: "33%", padding: "0 4px" }}>
+          <img
+            style={{
+              verticalAlign: "middle",
+              width: "100%",
+            }}
+            src={ich2}
+          />
+        </div>
+      </div>
+    </>
   </Layout>
 )
 
